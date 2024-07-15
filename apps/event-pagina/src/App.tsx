0f8a97d5e24e277e15fdcdfb7c78d9862637635c
raulfernandez <raul.fernandezvandijk@gmail.com>
2024-07-15 17:23:02 +0200

    Add two apps

---
+++ apps/event-pagina/src/App.tsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import './App.css'
+import "./assets/fonts/Poppins/Poppins-Medium.ttf";
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1 className={"text-6xl"}>Vegro Developer Festival</h1>
+        </>
+    )
+}
+
+export default App
